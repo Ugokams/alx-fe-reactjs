15bfb1c74aa09bdfe72d42fb01fe7e5bc144dc87
Nellydasylva <ugokamslink@gmail.com>
2026-01-10 11:56:36 +0100

    Profile card design using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,19 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <>      
       <div>
-        <Header />
-      </div>
-      
-      <div>
-        <MainContent />
+        <UserProfile 
+          name="Nelson Sylvanus"
+          age={35}
+          bio="Frontend engineer learning React at ALX."
+        />
+
       </div>
 
       <div>
