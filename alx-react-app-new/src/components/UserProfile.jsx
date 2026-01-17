5b289e94b25ac9466d6f74b54a0f7fe0caa2726b
Nellydasylva <ugokamslink@gmail.com>
2026-01-17 22:49:40 +0100

    css styling has been added to the react components

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 const UserProfile = (props) => {
     return(
         <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', backgroundColor:'orange' }}>
-            <h2 style={{ color: 'blue', fontSize: '20px' }}>Name: {props.name} </h2>
-            <p style={{ color: 'blue', fontWeight: 'bold', fontSize: '17px' }}> Age: {props.age} </p>
-            <p style= {{ color: 'blue', fontWeight: 'normal', fontSize: '17px'}}> Bio: {props.bio}</p>
+        <h2 style={{ color: 'blue' }}>{props.name}</h2>
+        <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+        <p>Bio: {props.bio}</p>
         </div>
     );
 };
