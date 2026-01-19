7d0bc2e219cd0f6f707ca91c418d0ce200272ef4
Nellydasylva <ugokamslink@gmail.com>
2026-01-19 19:32:46 +0100

    initial commit

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "15px",
+        backgroundColor: "#1e293b",
+        display: "flex",
+        gap: "20px",
+        margin: 0,
+      }}
+    >
+      <Link style={{ color: "white" }} to="/">Home</Link>
+      <Link style={{ color: "white" }} to="/about">About</Link>
+      <Link style={{ color: "white" }} to="/services">Services</Link>
+      <Link style={{ color: "white" }} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
