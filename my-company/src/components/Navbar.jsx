6aa53dca0ae45eed00cc03f2ea3e96c120e01746
Nellydasylva <ugokamslink@gmail.com>
2026-01-19 20:05:23 +0100

    Justify content has been added to th inline style of Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ function Navbar() {
         padding: "15px",
         backgroundColor: "#1e293b",
         display: "flex",
+        justifyContent: "center",
         gap: "20px",
         margin: 0,
       }}
