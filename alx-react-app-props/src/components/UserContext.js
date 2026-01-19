bc043cef3cbafe09dd26887529839be1c7173aea
Nellydasylva <ugokamslink@gmail.com>
2026-01-19 20:58:05 +0100

    Usercontext has been updated to meet the requirement

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,19 +1,18 @@
-import React, { createContext } from "react";
+import { createContext, useState } from "react";
 
-// Create the context
-export const UserContext = createContext();
+const UserContext = createContext();
 
-// Create a provider component
-export const UserProvider = ({ children }) => {
-  const user = {
-    name: "Nelson Sylvanus",
-    age: 35,
-    bio: "Aspiring full stack engineer"
-  };
+export function UserProvider({ children }) {
+  const [user, setUser] = useState({
+    name: "John Doe",
+    email: "john@example.com"
+  });
 
   return (
-    <UserContext.Provider value={user}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
-};
+}
+
+export default UserContext;
